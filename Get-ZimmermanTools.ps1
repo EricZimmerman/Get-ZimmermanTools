cd87f247df1bfef77a460111b62e865c23d372f7
Eric Zimmerman <saericzimmerman@gmail.com>
2019-01-22 17:09:13 -0500

    make dest mandatory

--- Get-ZimmermanTools.ps1
+++ Get-ZimmermanTools.ps1
@@ -1,8 +1,24 @@
+<#
+.SYNOPSIS
+    Ths script will discover and download all available programs from https://ericzimmerman.github.io and download them to $Dest
+.DESCRIPTION
+    A file will also be created in $Dest that tracks the SHA-1 of each file, so rerunning the script will only download new versions. To redownload, remove lines from or delete the CSV file created under $Dest and rerun.
+.PARAMETER Dest
+    The path you want to save the programs to.
+.EXAMPLE
+    C:\PS> Get-ZimmermanTools.ps1 -Dest c:\tools
+    Downloads/extracts and saves details about programs to c:\tools directory.
+.NOTES
+    Author: Eric Zimmerman
+    Date:   January 22, 2019    
+#>
+
 [Cmdletbinding()]
 # Where to extract the files to
 Param
 (
-    $Dest
+    [Parameter(Mandatory=$true)]
+    $Dest #Where to save programs to	
 )
 
 Write-Host "`nThs script will discover and download all available programs from https://ericzimmerman.github.io and download them to $Dest" -BackgroundColor Blue
