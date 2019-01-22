bcea409ae7f9939db497a1451b12fd40b7f09267
Eric <saericzimmerman@gmail.com>
2019-01-22 14:23:32 -0500

    Update Get-ZimmermanTools.ps1

--- Get-ZimmermanTools.ps1
+++ Get-ZimmermanTools.ps1
@@ -5,13 +5,6 @@ Param
     $Dest
 )
 
-function Unzip
-{
-    param([string]$zipfile, [string]$outpath)
-
-    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
-}
-
 Write-Host "`nThs script will discover and download all available programs from https://ericzimmerman.github.io and download them to $Dest" -BackgroundColor Blue
 Write-Host "A file will also be created in $Dest that tracks the SHA-1 of each file, so rerunning the script will only download new versions"
 Write-Host "To redownload, remove lines from or delete the CSV file created under $Dest and rerun. Enjoy!`n"
